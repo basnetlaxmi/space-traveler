585701f12a68b06a7ee3c16bf8f2fa5c77c091f0
Laxmiba7 <oluxmee@gmail.com>
2022-04-12 21:08:02 +0545

    Add api url of mission

--- src/Redux/apiFunctions.js
+++ src/Redux/apiFunctions.js
@@ -1,5 +1,8 @@
 const rockets = 'https://api.spacexdata.com/v3/rockets';
+const missions = 'https://api.spacexdata.com/v3/missions';
 
 const loadRockets = async () => rockets;
+
+const fetchMissions = async () => missions;
 
 export default loadRockets;

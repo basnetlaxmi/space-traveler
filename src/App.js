6b3e1ffd1b4ef61b90dc7be08b8e6e8f5ebc9455
unknown <saied2421998@gmail.com>
2022-04-12 17:17:43 +0200

    get the rockets from the api

--- src/App.js
+++ src/App.js
@@ -1,18 +1,25 @@
 import { Routes, Route } from 'react-router-dom';
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { rocketLoad } from './Redux/Rockets/Rockets';
 import Navbar from './components/Navbar/Navbar';
 import Rockets from './components/Rockets/Rockets';
 import Missions from './components/Missions/Missions';
 import Profile from './components/Profile/Profile';
 
-const App = () => (
-  <>
-    <Navbar />
-    <Routes>
-      <Route path="/" element={Rockets} />
-      <Route path="missions" element={Missions} />
-      <Route path="profile" element={Profile} />
-    </Routes>
-  </>
-);
+const App = () => {
+  const dispatch = useDispatch();
+  useEffect(() => dispatch(rocketLoad()), []);
+  return (
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Rockets />} />
+        <Route path="missions" element={<Missions />} />
+        <Route path="profile" element={<Profile />} />
+      </Routes>
+    </>
+  );
+};
 
 export default App;
--- src/components/Rockets/Rockets.js
+++ src/components/Rockets/Rockets.js
@@ -1,3 +1,13 @@
-const Rockets = () => null;
+import { useSelector, shallowEqual } from 'react-redux';
+import './Rockets.css';
+
+const Rockets = () => {
+  const rockets = useSelector((state) => state.rocketsReducer.rockets, shallowEqual);
+  return (
+    <ul id="rocket-list">
+      {rockets.map((rocket) => <li key={rocket.id}>{`${rocket.name} ${rocket.type} loaded`}</li>)}
+    </ul>
+  );
+};
 
 export default Rockets;

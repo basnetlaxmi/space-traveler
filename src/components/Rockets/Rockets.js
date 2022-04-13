a58363f7de3b1b7e9f554d93ce5ce38f367ae119
unknown <saied2421998@gmail.com>
2022-04-13 14:19:30 +0200

    Finish style & display rockets section

--- src/components/Rockets/Rockets.js
+++ src/components/Rockets/Rockets.js
@@ -9,8 +9,8 @@ const Rockets = () => {
       rockets.map((rocket) => (
         <li key={rocket.id} className="rocket-card">
           <img alt="rocket img" src={rocket.flickr_images[0]} />
-          <p>{rocket.name}</p>
-          <p>{rocket.description}</p>
+          <p className="rocket-name">{rocket.name}</p>
+          <p className="rocket-description">{rocket.description}</p>
           <button type="button">Reserve Rocket</button>
         </li>
       ))

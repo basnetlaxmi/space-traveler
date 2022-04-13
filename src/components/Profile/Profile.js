5363873674a7f45a2fa36392e329e641c0f8c45f
unknown <saied2421998@gmail.com>
2022-04-13 17:32:42 +0200

    Finish Profile Page

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,3 +1,22 @@
-const Profile = () => null;
+import { useSelector } from 'react-redux';
+import './Profile.css';
+
+const Profile = () => {
+  const reservedRockets = useSelector(
+    (state) => state.rocketsReducer.rockets.filter((rocket) => rocket.reserved === true),
+  );
+  return (
+    <div className="reserved-rockets-container">
+      <h2>My Rockets</h2>
+      <ul className="reserved-rockets">
+        {
+         reservedRockets.length
+           ? reservedRockets.map((rocket) => <li key={rocket.id}><p>{rocket.name}</p></li>)
+           : <li><p>{'You didn\'t reserve any Rocket yet.'}</p></li>
+        }
+      </ul>
+    </div>
+  );
+};
 
 export default Profile;

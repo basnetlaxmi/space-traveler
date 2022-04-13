c183f490f86f9e4cfab442fb88cb6405dcbb02bf
unknown <saied2421998@gmail.com>
2022-04-13 16:35:32 +0200

    finish reserve component view (badge % cancel button) & all reserved functionalitiy (true|false)

--- src/components/Rockets/Rockets.js
+++ src/components/Rockets/Rockets.js
@@ -5,6 +5,16 @@ import { reserveRocket } from '../../Redux/Rockets/Rockets';
 const Rockets = () => {
   const rockets = useSelector((state) => state.rocketsReducer.rockets, shallowEqual);
   const dispatch = useDispatch();
+  const reserveBtnStyle = {
+    color: '#fff',
+    border: '1px solid #007bff',
+    background: '#007bff',
+  };
+  const cancelReserveBtnStyle = {
+    color: '#6d757d',
+    border: '1px solid #6d757d',
+    background: '#fff',
+  };
   return (
     <ul id="rocket-list">
       {
@@ -12,9 +22,18 @@ const Rockets = () => {
         <li key={rocket.id} className="rocket-card">
           <img alt="rocket img" src={rocket.flickr_images[0]} />
           <p className="rocket-name">{rocket.name}</p>
-          <p className="rocket-description">{rocket.description}</p>
-          <button onClick={(e, id = rocket.id) => dispatch(reserveRocket(id))} type="button">
-            Reserve Rocket
+          <p className="rocket-description">
+            <span className={`badge ${rocket.reserved ? '' : 'dn'}`}>
+              Reserved
+            </span>
+            {rocket.description}
+          </p>
+          <button
+            onClick={(e, id = rocket.id) => dispatch(reserveRocket(id))}
+            style={rocket.reserved ? cancelReserveBtnStyle : reserveBtnStyle}
+            type="button"
+          >
+            { rocket.reserved ? 'Cancel Reservation' : 'Reserve Rocket'}
           </button>
         </li>
       ))

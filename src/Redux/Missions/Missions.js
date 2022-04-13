c650b60e72cb456cf1540ad8b553cda89b74add4
Laxmiba7 <oluxmee@gmail.com>
2022-04-13 16:31:19 +0545

    Fetch mission api

--- src/Redux/Missions/Missions.js
+++ src/Redux/Missions/Missions.js
@@ -2,22 +2,26 @@ import { fetchMissionsData } from '../apiFunctions';
 // Constant
 const FETCH_MISSIONS = 'space-traveler/Missions/FETCH_MISSIONS';
 // Action
-export const fetchMission = () => (dispatch) => {
-  const res = fetchMissionsData();
-  console.log(res);
+export const fetchMission = () => async (dispatch) => {
+  const res = await fetchMissionsData();
+  // console.log(res);
+  dispatch({ type: FETCH_MISSIONS, payload: res });
 };
 
 // Reducers
 const initialState = {
   missions: [],
+
 };
 const missionReducer = (state = initialState, action) => {
   switch (action.type) {
     case FETCH_MISSIONS:
-        return {
-            ...state,
-            missions: action.payload
-        }
+      return {
+        ...state,
+        missions: action.payload,
+
+      };
+
     default:
       return state;
   }
--- src/Redux/apiFunctions.js
+++ src/Redux/apiFunctions.js
@@ -4,9 +4,15 @@ const missions = 'https://api.spacexdata.com/v3/missions';
 const loadRockets = async () => rockets;
 
 const fetchMissionsData = async () => {
-    const response = await fetch(missions).then((res) => res.json()).then((result)=> result);
-    return response;
-   
-}
+  const response = await fetch(missions).then((res) => res.json()).then((result) => result);
+  const missionArr = response.map((mission) => (
+    {
+      id: mission.mission_id,
+      name: mission.mission_name,
+      description: mission.description,
+    }
+  ));
+  return missionArr;
+};
 // fetchMissionsData();
 export { loadRockets, fetchMissionsData };

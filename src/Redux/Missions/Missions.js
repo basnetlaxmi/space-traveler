4a1c8ef23770e5e738b6057feb526b8b9e326e00
Laxmiba7 <oluxmee@gmail.com>
2022-04-14 07:30:59 +0545

    Add redux for joining mission

--- src/Redux/Missions/Missions.js
+++ src/Redux/Missions/Missions.js
@@ -1,11 +1,16 @@
 import { fetchMissionsData } from '../apiFunctions';
 // Constant
 const FETCH_MISSIONS = 'space-traveler/Missions/FETCH_MISSIONS';
+const JOIN_MISSION = 'space-traveler/Missions/JOIN_MISSION';
 // Action
 export const fetchMission = () => async (dispatch) => {
   const res = await fetchMissionsData();
   dispatch({ type: FETCH_MISSIONS, payload: res });
 };
+
+export const joinMission = (id) => async(dispatch) => {
+  dispatch({type: joinMission, payload: id})
+}
 
 // Reducers
 const initialState = {
@@ -20,6 +25,9 @@ const missionReducer = (state = initialState, action) => {
         missions: action.payload,
 
       };
+      case JOIN_MISSION:
+        const findItem = state.missions.find((item) => item.id === action.payload.id)
+        console.log(findItem);
 
     default:
       return state;

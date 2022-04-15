e328f3192ef052e6140805a794fca0c83eff9da8
Laxmiba7 <oluxmee@gmail.com>
2022-04-15 12:50:12 +0545

    Add table for mission in profile page

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -6,15 +6,27 @@ const Profile = () => {
     (state) => state.rocketsReducer.rockets.filter((rocket) => rocket.reserved === true),
   );
   return (
-    <div className="reserved-rockets-container">
-      <h2>My Rockets</h2>
-      <ul className="reserved-rockets">
-        {
+    <div className="d-flex">
+      <div className="reserved-rockets-container">
+        <h2>My Rockets</h2>
+        <ul className="reserved-rockets">
+          {
          reservedRockets.length
            ? reservedRockets.map((rocket) => <li key={rocket.id}><p>{rocket.name}</p></li>)
            : <li><p>{'You didn\'t reserve any Rocket yet.'}</p></li>
         }
-      </ul>
+        </ul>
+      </div>
+      <div className="reserved-rockets-container ms-5">
+        <h2>My Missions</h2>
+        <ul className="reserved-rockets">
+          {
+        reservedRockets.length
+          ? reservedRockets.map((rocket) => <li key={rocket.id}><p>{rocket.name}</p></li>)
+          : <li><p>{'You didn\'t reserve any Rocket yet.'}</p></li>
+       }
+        </ul>
+      </div>
     </div>
   );
 };
